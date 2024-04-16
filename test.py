e331fb3ac0ad46a037aece430b596837ccbd48fc
Thomas Warford <thomasdwarford@hotmail.co.uk>
2024-04-16 18:15:38 +0100

    formatting

--- test.py
+++ test.py
@@ -6,19 +6,16 @@ def test_linear_slice():
     element = structure_graph.structure.elements[0].name
     sites = structure_graph.structure.sites
 
-
     three = linear_slice(sites[1].frac_coords, sites[5].frac_coords, (0, 1, 0), chgcar)
     # print(3, three)
     four = linear_slice(sites[3].frac_coords, sites[7].frac_coords, (1, 0, 0), chgcar)
     # print(4, four)
     assert(np.isclose(three, four).all())
-    del three, four
 
 def test_linear_slices():
     structure_graph, chgcar = create_structure_graph("mp-25")
     element = structure_graph.structure.elements[0].name
     sites = structure_graph.structure.sites
-
 
     out = linear_slices(
         [sites[1].frac_coords, sites[3].frac_coords],
