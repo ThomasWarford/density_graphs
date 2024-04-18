6708d9b043fab38499341b8e1f85efeb6a4e8c65
Thomas Warford <thomasdwarford@hotmail.co.uk>
2024-04-18 11:20:40 +0100

    Slices from mst

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ import pandas as pd
 if __name__ == "__main__":
     df = pd.read_csv("data/df_material.csv", index_col="material_id")
     df = df[["formula", "sublattice_element"]]
-    df = get_mst_slices_from_material_ids(df)
+    df = get_mst_slices_from_material_ids(df, n=30)
     df.to_csv("data/df_slices.csv")
